f5f3e80f0d50cd489754c25542c7bda21257a3c4
Tên của bạn <Email ca bn>
2026-01-08 12:38:54 +0700

    Initial commit

---
+++ src/test/java/vochikien1/Vochikien1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vochikien1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vochikien1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
